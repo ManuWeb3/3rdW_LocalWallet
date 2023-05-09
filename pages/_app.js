8cb8eb4e303c5e783a732a838d5c31f741db6172
Manu <manu.web3.0@gmail.com>
2023-05-09 17:34:58 -0600

    1st commit - LocalWallet setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, walletConnect, walletConnectV1 } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider 
+    activeChain={activeChain}
+    supportedWallets={[
+      metamaskWallet(),
+      // coinbaseWallet(),
+      // walletConnectV1(),
+      localWallet(),
+      ]}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,17 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+        <ConnectWallet/>
+        <Web3Button
+          contractAddress="0x16433D082e8b7F8514FAcbB3c066bcD751769b67"
+          action={(contract) => contract.erc721.claim(1)}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+          Claim
+        </Web3Button>
       </main>
     </div>
   );
